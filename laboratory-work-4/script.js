2f7b9252e034f0d648bea5375ce9e302b8e0157d
TangiresH <dimon4ik1010@gmail.com>
2023-12-03 21:49:23 +0200

    completed the second task of the fourth laboratory assignment

--- laboratory-work-4/script.js
+++ laboratory-work-4/script.js
@@ -1,15 +1,21 @@
 const myNameElement = document.getElementById('my-name');
 const birthplaceElement = document.querySelector('#birthplace');
+const buttonsContainer = document.querySelector('.buttons-container');
 
-
-myNameElement.addEventListener('click', function() {
+myNameElement.addEventListener('click', function () {
     changeColors(myNameElement);
 });
 
-birthplaceElement.addEventListener('click', function() {
+birthplaceElement.addEventListener('click', function () {
     changeColors(birthplaceElement);
 });
 
+buttonsContainer.addEventListener('click', function (event) {
+    const targetButton = event.target;
+    if (targetButton.tagName === 'BUTTON') {
+        handleButtonClick(targetButton);
+    }
+});
 
 function changeColors(element) {
     const randomBgColor = getRandomColor();
@@ -26,4 +32,47 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function addPhotoButton() {
+    const newPhoto = document.createElement('img');
+    newPhoto.src = 'https://sofitel.accor.com/destinations/imagerie/florence-city-photo-1400x787-877d_1400x788.jpg'; // Замініть на фактичний URL зображення
+    newPhoto.alt = 'photo';
+    newPhoto.classList.add('photo-city');
+    document.body.appendChild(newPhoto);
+}
+
+function handleButtonClick(button) {
+    const photos = document.querySelectorAll('.photo-city');
+    const lastPhoto = photos[photos.length - 1];
+
+    switch (button.dataset.action) {
+        case 'addPhoto':
+            addPhotoButton();
+            break;
+        case 'enlargePhoto':
+            modifyPhotoSize(lastPhoto, 1.2);
+            break;
+        case 'reducePhoto':
+            modifyPhotoSize(lastPhoto, 0.8);
+            break;
+        case 'deletePhoto':
+            deletePhotoButton(lastPhoto);
+            break;
+    }
+}
+
+function modifyPhotoSize(photo, factor) {
+    if (photo) {
+        const currentWidth = parseFloat(window.getComputedStyle(photo).width);
+        const currentHeight = parseFloat(window.getComputedStyle(photo).height);
+        photo.style.width = `${currentWidth * factor}px`;
+        photo.style.height = `${currentHeight * factor}px`;
+    }
+}
+
+function deletePhotoButton(photo) {
+    if (photo) {
+        photo.remove();
+    }
 }
